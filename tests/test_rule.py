acfc8dba7df1f1e74f594ea57e05145364cb6e25
bsmukasa <bsmukasa@gmail.com>
2015-12-16 16:42:41 -0500

    Add test for AndRule if components are made of compound AndRule instances.

--- tests/test_rule.py
+++ tests/test_rule.py
@@ -58,7 +58,11 @@ class TestAndRule(TestCase):
         msft.update(datetime(2014, 2, 11), 12)
         redhat = Stock("RHT")
         redhat.update(datetime(2014, 2, 12), 7)
-        cls.exchange = {"GOOG": goog, "MSFT": msft, "RHT": redhat}
+        apple = Stock("AAPL")
+        apple.update(datetime(2014, 2, 13), 20)
+        yahoo = Stock("YHOO")
+        yahoo.update(datetime(2014, 2, 14), 9)
+        cls.exchange = {"GOOG": goog, "MSFT": msft, "RHT": redhat, "AAPL": apple, "YHOO": yahoo}
 
     def test_an_AndRule_matches_if_all_component_rules_are_true(self):
         """Tests if True is returned if all component rules of a AndRule are true.
@@ -89,7 +93,19 @@ class TestAndRule(TestCase):
         """Tests if False is returned if all component rules of a AndRule are false and one component is an AndRule.
 
         """
-        and_rule = AndRule(PriceRule("GOOG", lambda stock: stock.price < 8),
+        and_rule = AndRule(PriceRule("GOOG", lambda stock: stock.price > 8),
                            PriceRule("MSFT", lambda stock: stock.price > 10))
-        rule = AndRule(and_rule, PriceRule("RHT", lambda stock: stock.price > 5))
+        rule = AndRule(and_rule, PriceRule("RHT", lambda stock: stock.price < 5))
         self.assertFalse(rule.matches(self.exchange))
+
+    def test_an_AndRule_true_matches_if_components_made_of_several_AndRules(self):
+        """Tests if True is returned if all component rules of a AndRule are compound AndRules.
+
+        """
+        and_rule1 = AndRule(PriceRule("GOOG", lambda stock: stock.price > 8),
+                            PriceRule("MSFT", lambda stock: stock.price > 10))
+        and_rule2 = AndRule(and_rule1, PriceRule("RHT", lambda stock: stock.price > 5))
+        and_rule3 = AndRule(PriceRule("AAPL", lambda stock: stock.price > 19),
+                            PriceRule("YHOO", lambda stock: stock.price < 10))
+        rule = AndRule(and_rule2, and_rule3)
+        self.assertTrue(rule.matches(self.exchange))
