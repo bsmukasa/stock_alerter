7a0007c92c023e34bf237d206f2e308cd18e3c67
bsmukasa <bsmukasa@gmail.com>
2015-12-16 14:27:58 -0500

    Add stock update test.

--- tests/test_stock.py
+++ tests/test_stock.py
@@ -1,4 +1,6 @@
 import unittest
+from datetime import datetime
+
 from stock import Stock
 
 
@@ -7,6 +9,15 @@ class StockTest(unittest.TestCase):
         stock = Stock("GOOG")
         self.assertIsNone(stock.price)
 
+    def test_stock_update(self):
+        """An update should set the price on the stock object.
+
+        Notes:
+            We will be using the `datetime` module for the timestamp.
+        """
+        goog = Stock("GOOG")
+        goog.update(datetime(2014, 2, 12), price=10)
+        self.assertEqual(10, goog.price)
 
 if __name__ == "__main__":
     unittest.main()
