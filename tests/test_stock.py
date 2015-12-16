4aa050aa12873859e0b65a3353d985386badf83c
bsmukasa <bsmukasa@gmail.com>
2015-12-16 16:43:38 -0500

    Add test to check is price returned is latest per timestamp.

--- tests/test_stock.py
+++ tests/test_stock.py
@@ -41,6 +41,17 @@ class StockTest(unittest.TestCase):
         self.stock.update(datetime(2014, 2, 12), price=23.12)
         self.assertAlmostEqual(23.12, self.stock.price, places=4)
 
+    def test_stock_returns_latest_timestamp_price(self):
+        """A stock should return the latest price per timestamp.
+
+        """
+        self.stock.update(datetime(2014, 2, 12), price=10)
+        self.stock.update(datetime(2014, 2, 10), price=10.2)
+        self.stock.update(datetime(2014, 2, 15), price=15.789)
+        self.stock.update(datetime(2014, 2, 11), price=18.236458)
+        self.stock.update(datetime(2014, 2, 9), price=23.12)
+        self.assertAlmostEqual(15.789, self.stock.price, places=4)
+
 
 class StockTrendTest(unittest.TestCase):
     def setUp(self):
