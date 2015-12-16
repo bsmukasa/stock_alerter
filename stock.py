d1e9e743356e5d4f508168f10403eea1953669ab
bsmukasa <bsmukasa@gmail.com>
2015-12-16 14:11:21 -0500

    Move StockTest class from stock script to test_stock script.

--- stock.py
+++ stock.py
@@ -1,4 +1,4 @@
-import unittest
+
 
 
 class Stock:
@@ -7,10 +7,4 @@ class Stock:
         self.price = None
 
 
-class StockTest(unittest.TestCase):
-    def test_price_of_a_new_stock_class_should_be_None(self):
-        stock = Stock("GOOG")
-        self.assertIsNone(stock.price)
 
-if __name__ == "__main__":
-    unittest.main()
