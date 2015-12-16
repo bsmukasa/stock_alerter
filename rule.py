74888d07942c0ee9ab8accbe87732380a700f9d0
bsmukasa <bsmukasa@gmail.com>
2015-12-16 15:39:33 -0500

    Add PriceRule class and init method.

--- rule.py
+++ rule.py
@@ -1,1 +1,9 @@
+class PriceRule:
+    """PriceRule is a rule that triggers when a stock price satisfies a condition.
 
+    The condition is usually greater, equal or lesser than a given value.
+    """
+
+    def __init__(self, symbol, condition):
+        self.symbol = symbol
+        self.condition = condition
