d6dc372b69a73b81f63c2ed7a8fe3cd3fde7012a
bsmukasa <bsmukasa@gmail.com>
2015-12-30 17:19:18 -0500

    Add constructor, __getitem__ and update methods to TimeSeries class.

--- timeseries.py
+++ timeseries.py
@@ -1,2 +1,15 @@
+import bisect
+import collections
+
+Update = collections.namedtuple("Update", ["timestamp", "value"])
+
+
 class TimeSeries:
-    pass
+    def __init__(self):
+        self.series = []
+
+    def __getitem__(self, index):
+        return self.series[index]
+
+    def update(self, timestamp, value):
+        bisect.insort_left(self.series, Update(timestamp, value))
