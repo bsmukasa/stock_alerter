bc7aecd5d24656ecfe0c11decd740150b7ad0f8b
bsmukasa <bsmukasa@gmail.com>
2016-01-04 14:22:12 -0500

    Add Email Action class and execute method.

--- action.py
+++ action.py
@@ -1,4 +1,26 @@
+import smtplib
+from email.mime.text import MIMEText
+
+
 class PrintAction:
     @staticmethod
     def execute(content):
         print(content)
+
+
+class EmailAction:
+    from_email = "alerts@stocks.com"
+
+    def __init__(self, to):
+        self.to_email = to
+
+    def execute(self, content):
+        message = MIMEText(content)
+        message["Subject"] = "New Stock Alert"
+        message["From"] = "alerts@stocks.com"
+        message["To"] = self.to_email
+        smtp = smtplib.SMTP("email.stocks.com")
+        try:
+            smtp.send_message(message)
+        finally:
+            smtp.quit()
