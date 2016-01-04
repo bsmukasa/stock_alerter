5e3c6d6ab892a87ca27c05c01b39646bd339b3f2
bsmukasa <bsmukasa@gmail.com>
2016-01-04 12:49:31 -0500

    Refactor a lightweight Mock class.

--- tests/test_event.py
+++ tests/test_event.py
@@ -2,27 +2,27 @@ import unittest
 from event import Event
 
 
+class Mock:
+    def __init__(self):
+        self.called = False
+        self.params = ()
+
+    def __call__(self, *args, **kwargs):
+        self.called = True
+        self.params = (args, kwargs)
+
+
 class EventTest(unittest.TestCase):
     def test_a_listener_is_notified_when_event_is_raised(self):
-        called = False
-
-        def listener():
-            nonlocal called
-            called = True
-
+        listener = Mock()
         event = Event()
         event.connect(listener)
         event.fire()
-        self.assertTrue(called)
+        self.assertTrue(listener.called)
 
     def test_a_listener_is_passed_correct_parameters(self):
-        params = ()
-
-        def listener(*args, **kwargs):
-            nonlocal params
-            params = (args, kwargs)
-
+        listener = Mock()
         event = Event()
         event.connect(listener)
         event.fire(5, shape="square")
-        self.assertEquals(((5, ), {"shape": "square"}), params)
+        self.assertEquals(((5, ), {"shape": "square"}), listener.params)
