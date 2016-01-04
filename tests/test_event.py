2b22503a82db9fbc69ff8af08c30c3574bde1831
bsmukasa <bsmukasa@gmail.com>
2016-01-04 12:51:01 -0500

    Refactor to use unittest.mock

--- tests/test_event.py
+++ tests/test_event.py
@@ -1,28 +1,19 @@
 import unittest
+from unittest import mock
 from event import Event
-
-
-class Mock:
-    def __init__(self):
-        self.called = False
-        self.params = ()
-
-    def __call__(self, *args, **kwargs):
-        self.called = True
-        self.params = (args, kwargs)
 
 
 class EventTest(unittest.TestCase):
     def test_a_listener_is_notified_when_event_is_raised(self):
-        listener = Mock()
+        listener = mock.Mock()
         event = Event()
         event.connect(listener)
         event.fire()
         self.assertTrue(listener.called)
 
     def test_a_listener_is_passed_correct_parameters(self):
-        listener = Mock()
+        listener = mock.Mock()
         event = Event()
         event.connect(listener)
         event.fire(5, shape="square")
-        self.assertEquals(((5, ), {"shape": "square"}), listener.params)
+        listener.assert_called_with(5, shape="square")
